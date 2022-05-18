b5428fbececcd813caf1c47f1f349f75627c3706
yuliatokaryk <julia4099@gmail.com>
2022-05-18 10:09:13 +0300

    BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,28 +6,12 @@ require_relative 'base_controller'
 # Books Controller
 class BooksController < BaseController
   def index
-    headers = { 'Content-Type' => 'application/json' }
-
     [
       200,
-      headers,
-      [index_body]
+      { 'Content-Type' => 'application/json' },
+      [
+        Database.new.connection.execute('SELECT * FROM books').to_json
+      ]
     ]
-  end
-
-  private
-
-  def index_body
-    books = Database.new.connection.execute('SELECT * FROM books')
-    books_json = {}
-
-    books.each do |book|
-      books_json[book['id']] = {
-        name: book['name'],
-        author: book['author']
-      }
-    end
-
-    books_json.to_json
   end
 end
