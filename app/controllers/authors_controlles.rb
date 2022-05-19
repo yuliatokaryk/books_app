fb1bdad1e3896b597276dd4686ebea8b42eff595
yuliatokaryk <julia4099@gmail.com>
2022-05-19 16:25:54 +0300

    authors show added

--- app/controllers/authors_controlles.rb
+++ app/controllers/authors_controlles.rb
@@ -12,6 +12,24 @@ class AuthorsController < BaseController
     ]
   end
 
+  def show(id)
+    authors = db.execute("SELECT * FROM authors WHERE id = #{id}")
+
+    if authors == []
+      [
+        404,
+        { 'Content-Type' => 'text/plain' },
+        ["authors #{id} not found"]
+      ]
+    else
+      [
+        200,
+        { 'Content-Type' => 'application/json' },
+        [authors.to_json]
+      ]
+    end
+  end
+
   def create(params:)
     db.execute("INSERT INTO authors (name, country) VALUES('#{params['name']}', '#{params['country']}')")
     [
